296d7e28fb622e9cfb780c70573e80c7cd69b3db
CHRISTIAN HIRUELAS GALLARDO <dev.christianhiruelasgallardo@gmail.com>
2026-04-17 16:08:32 +0200

    affichage de la table task

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TaskController extends AbstractController
 {
-    #[Route('/task', name: 'app_task')]
-    public function index(): Response
+    #[Route('/', name: 'app_task')]
+    public function index(TaskRepository $repo): Response
     {
+
+        // $tasks= $repo->findAll();
+        // dd($tasks);
+
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'tasks'=> $repo->findAll()
         ]);
     }
 }
