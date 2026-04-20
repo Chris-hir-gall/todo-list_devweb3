3b9c0a591ea7fbf9eff2f2f89269bf3f022b3675
CHRISTIAN HIRUELAS GALLARDO <dev.christianhiruelasgallardo@gmail.com>
2026-04-20 12:51:09 +0200

    form for task entity + new category entity

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,22 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
 use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TaskController extends AbstractController
 {
     #[Route('/', name: 'app_task')]
-    public function index(TaskRepository $repo): Response
+    public function index(Request $request, EntityManagerInterface $em, TaskRepository $repo): Response
     {
 
-        // $tasks= $repo->findAll();
-        // dd($tasks);
+       $task = new Task;
+       $form = $this->createForm(TaskType::class, $task);
+       $form->handleRequest($request);
+
+       if($form->isSubmitted()){
+        $task->setIsDone(false);
+        $task->setCreateAt(new \DateTimeImmutable());
+        $em->persist($task);
+        $em->flush();
+
+        return $this->redirectToRoute('app_task');
+       }
 
         return $this->render('task/index.html.twig', [
-            'tasks'=> $repo->findAll()
+            'tasks'=> $repo->findBy([],['createAt'=>'DESC']),
+            'form'=>$form
         ]);
     }
 }
